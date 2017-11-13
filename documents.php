c228330e59a9a2922ff1b494694fbe32b19a7e36
aliastim <33629825+aliastim@users.noreply.github.com>
2017-11-13 18:55:45 +0100

    ss

--- documents.php
+++ documents.php
@@ -11,51 +11,59 @@
 
 					<br>
 
-					<button type="button" class="btn btn-secondary btn-lg btn-block btn-docs">Block level button</button>
-					<button type="button" class="btn btn-secondary btn-lg btn-block btn-docs">Block level button</button>
-					<button type="button" class="btn btn-secondary btn-lg btn-block btn-docs">Block level button</button>
-					<button type="button" class="btn btn-secondary btn-lg btn-block btn-docs">Block level button</button>
+					<button type="button" class="btn btn-secondary btn-lg btn-block btn-docs">REGLEMENT INTERIEUR IIM 2017-2018</button>
+					<button type="button" class="btn btn-secondary btn-lg btn-block btn-docs">REGLEMENT PEDAGOGIQUE IIM 2017-2018</button>
+					<button type="button" class="btn btn-secondary btn-lg btn-block btn-docs">REGLEMENT INTERIEUR CG 92 applicable au 16/08/2016</button>
+					<button type="button" class="btn btn-secondary btn-lg btn-block btn-docs">CHARTE INFORMATIQUE ETUDIANT</button>
 
 					<br><br>
 
 					<div>
 						<input id="ac-1" name="accordion-1" type="checkbox" /><!-- attribut checked pour l'ouvrir automatiquement-->
 							<label for="ac-1" class="ovr">Scolarité</label>
-								<article class="ac-large350">
-									<img src="" class="img-responsive text-center center-block logoapropos" alt="Responsive image">
-									<p class="orange text-center"><B>Bienvenue sur Agora.orange</B><br><br> Agora est une idée née suite à la fermeture de feu T411, un site qui aura berçé nos débuts dans ce monde fantastique qu'est Internet. Nous avons eu l'idée de concevoir un site de Cloud où vous choisissez le contenu que vous souhaitez garder pour vous ou partager au reste de la communauté. Agora étant un petit site privé, nous vous demandons de rester discret sur ce dernier afin de restreindre la communauté. <br><br> Nous vous souhaitons une agréable visite !</p>
+								<article class="ac-small">
+									<p class="text-left txt-docs"><a href="#">Calendrier académique 2017-2018</a><br>
+									<a href="#">Guide d'usage HQ My print</a><br>
+									<a href="#">Equipe IIM</a><br>
+									<a href="#">Dates clés 2017 - 2018</a></p>
 								</article>
 					</div>
 					<div>
 						<input id="ac-3" name="accordion-3" type="checkbox" />
 							<label for="ac-3" class="ovr">Stages</label>
-								<article class="ac-large350">
-									<img src="" class="img-responsive text-center center-block logoapropos" alt="Responsive image">
-									<p class="orange text-center"><B>Bienvenue sur Agora.orange</B><br><br> Agora est une idée née suite à la fermeture de feu T411, un site qui aura berçé nos débuts dans ce monde fantastique qu'est Internet. Nous avons eu l'idée de concevoir un site de Cloud où vous choisissez le contenu que vous souhaitez garder pour vous ou partager au reste de la communauté. Agora étant un petit site privé, nous vous demandons de rester discret sur ce dernier afin de restreindre la communauté. <br><br> Nous vous souhaitons une agréable visite !</p>
+								<article class="ac-small">
+									<p class="text-left txt-docs"><a href="#">Lettre de mission en version anglaise</a><br>
+									<a href="#">LETTRE DE MISSION 2017-2018</a><br>
+									<a href="#">Comment obtenir votre convention de stage?</a></p>
 								</article>
 					</div>
 					<div>
 						<input id="ac-4" name="accordion-4" type="checkbox" />
 							<label for="ac-4" class="ovr">Général</label>
-								<article class="ac-large350">
-									<img src="" class="img-responsive text-center center-block logoapropos" alt="Responsive image">
-									<p class="orange text-center"><B>Bienvenue sur Agora.orange</B><br><br> Agora est une idée née suite à la fermeture de feu T411, un site qui aura berçé nos débuts dans ce monde fantastique qu'est Internet. Nous avons eu l'idée de concevoir un site de Cloud où vous choisissez le contenu que vous souhaitez garder pour vous ou partager au reste de la communauté. Agora étant un petit site privé, nous vous demandons de rester discret sur ce dernier afin de restreindre la communauté. <br><br> Nous vous souhaitons une agréable visite !</p>
+								<article class="ac-small">
+									<p class="text-left txt-docs"><a href="#">Que faire en cas de perte de badge?</a><br>
+									<a href="#">Guide des premiers pas des outils numériques</a><br>
+									<a href="#">Que faire en cas de perte de badge?</a><p>
 								</article>
 					</div>
 					<div>
 						<input id="ac-5" name="accordion-5" type="checkbox" />
 							<label for="ac-5" class="ovr">Vie Associative</label>
-								<article class="ac-large350">
-									<img src="" class="img-responsive text-center center-block logoapropos" alt="Responsive image">
-									<p class="orange text-center"><B>Bienvenue sur Agora.orange</B><br><br> Agora est une idée née suite à la fermeture de feu T411, un site qui aura berçé nos débuts dans ce monde fantastique qu'est Internet. Nous avons eu l'idée de concevoir un site de Cloud où vous choisissez le contenu que vous souhaitez garder pour vous ou partager au reste de la communauté. Agora étant un petit site privé, nous vous demandons de rester discret sur ce dernier afin de restreindre la communauté. <br><br> Nous vous souhaitons une agréable visite !</p>
+								<article class="ac-medium">
+									<p class="text-left txt-docs"><a href="#"> COFA : réglement</a><br>
+									<a href="#">PEE : guide de l'étudiant</a><br>
+									<a href="#">Créer une association</a><br>
+									<a href="#">Charte des associations et des clubs du groupe Léonard de Vinci</a><br>
+									<a href="#">Réglement des points de bonification</a><br>
+									<a href="#">COFA : demande de subvention</a></p>
 								</article>
 					</div>
 					<div>
 						<input id="ac-6" name="accordion-6" type="checkbox" />
 							<label for="ac-6" class="ovr">Sport</label>
-								<article class="ac-large350">
-									<img src="" class="img-responsive text-center center-block logoapropos" alt="Responsive image">
-									<p class="orange text-center"><B>Bienvenue sur Agora.orange</B><br><br> Agora est une idée née suite à la fermeture de feu T411, un site qui aura berçé nos débuts dans ce monde fantastique qu'est Internet. Nous avons eu l'idée de concevoir un site de Cloud où vous choisissez le contenu que vous souhaitez garder pour vous ou partager au reste de la communauté. Agora étant un petit site privé, nous vous demandons de rester discret sur ce dernier afin de restreindre la communauté. <br><br> Nous vous souhaitons une agréable visite !</p>
+								<article class="ac-small">
+									<p class="text-left txt-docs"><a href="#">Licence Universitaire</a><br>
+									<a href="#">Dossier Forest Hill</a></p>
 								</article>
 					</div>
 				
@@ -72,9 +80,18 @@
 					<div>
 						<input id="ac-2" name="accordion-2" type="checkbox" checked/>
 							<label for="ac-2" class="ovr">Fichiers Experts 2017</label>
-								<article class="ac-large350">
-									<img src="" class="img-responsive text-center center-block logoapropos" alt="Responsive image">
-									<p class="orange text-center"><B>Bienvenue sur Agora.orange</B><br><br> Agora est une idée née suite à la fermeture de feu T411, un site qui aura berçé nos débuts dans ce monde fantastique qu'est Internet. Nous avons eu l'idée de concevoir un site de Cloud où vous choisissez le contenu que vous souhaitez garder pour vous ou partager au reste de la communauté. Agora étant un petit site privé, nous vous demandons de rester discret sur ce dernier afin de restreindre la communauté. <br><br> Nous vous souhaitons une agréable visite !</p>
+								<article class="ac-small">
+									<table class="table ovr">
+									  <thead class="thead-inverse text-center">
+									    <tr>
+									      <th>Document</th>
+									      <th>Poids</th>
+									      <th>Téléchargement</th>
+									    </tr>
+									  </thead>
+									  <tbody class="text-center">									  
+									  </tbody>
+									</table>
 								</article>
 					</div>
 

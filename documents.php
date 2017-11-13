d606f7e68b18385ba7e4900fe38dfbbd32723232
aliastim <33629825+aliastim@users.noreply.github.com>
2017-11-13 18:26:57 +0100

    ii

--- documents.php
+++ documents.php
@@ -1,3 +1,128 @@
 <?php require 'menu.php' ?>
 
-<h1 class="text-center"> Test </h1>
+<!--<h1 class="text-center"> Test </h1>-->
+
+<div class="container">
+	<div class="row">
+		<div class="col-6 ctn1">
+
+			<section class="ac-container ctncolor ovr">
+					<h1 class="text-center">Documents</h1>
+
+					<br>
+
+					<button type="button" class="btn btn-secondary btn-lg btn-block btn-docs">Block level button</button>
+					<button type="button" class="btn btn-secondary btn-lg btn-block btn-docs">Block level button</button>
+					<button type="button" class="btn btn-secondary btn-lg btn-block btn-docs">Block level button</button>
+					<button type="button" class="btn btn-secondary btn-lg btn-block btn-docs">Block level button</button>
+
+					<br><br>
+
+					<div>
+						<input id="ac-1" name="accordion-1" type="checkbox" /><!-- attribut checked pour l'ouvrir automatiquement-->
+							<label for="ac-1" class="ovr">Scolarité</label>
+								<article class="ac-large350">
+									<img src="" class="img-responsive text-center center-block logoapropos" alt="Responsive image">
+									<p class="orange text-center"><B>Bienvenue sur Agora.orange</B><br><br> Agora est une idée née suite à la fermeture de feu T411, un site qui aura berçé nos débuts dans ce monde fantastique qu'est Internet. Nous avons eu l'idée de concevoir un site de Cloud où vous choisissez le contenu que vous souhaitez garder pour vous ou partager au reste de la communauté. Agora étant un petit site privé, nous vous demandons de rester discret sur ce dernier afin de restreindre la communauté. <br><br> Nous vous souhaitons une agréable visite !</p>
+								</article>
+					</div>
+					<div>
+						<input id="ac-3" name="accordion-3" type="checkbox" />
+							<label for="ac-3" class="ovr">Stages</label>
+								<article class="ac-large350">
+									<img src="" class="img-responsive text-center center-block logoapropos" alt="Responsive image">
+									<p class="orange text-center"><B>Bienvenue sur Agora.orange</B><br><br> Agora est une idée née suite à la fermeture de feu T411, un site qui aura berçé nos débuts dans ce monde fantastique qu'est Internet. Nous avons eu l'idée de concevoir un site de Cloud où vous choisissez le contenu que vous souhaitez garder pour vous ou partager au reste de la communauté. Agora étant un petit site privé, nous vous demandons de rester discret sur ce dernier afin de restreindre la communauté. <br><br> Nous vous souhaitons une agréable visite !</p>
+								</article>
+					</div>
+					<div>
+						<input id="ac-4" name="accordion-4" type="checkbox" />
+							<label for="ac-4" class="ovr">Général</label>
+								<article class="ac-large350">
+									<img src="" class="img-responsive text-center center-block logoapropos" alt="Responsive image">
+									<p class="orange text-center"><B>Bienvenue sur Agora.orange</B><br><br> Agora est une idée née suite à la fermeture de feu T411, un site qui aura berçé nos débuts dans ce monde fantastique qu'est Internet. Nous avons eu l'idée de concevoir un site de Cloud où vous choisissez le contenu que vous souhaitez garder pour vous ou partager au reste de la communauté. Agora étant un petit site privé, nous vous demandons de rester discret sur ce dernier afin de restreindre la communauté. <br><br> Nous vous souhaitons une agréable visite !</p>
+								</article>
+					</div>
+					<div>
+						<input id="ac-5" name="accordion-5" type="checkbox" />
+							<label for="ac-5" class="ovr">Vie Associative</label>
+								<article class="ac-large350">
+									<img src="" class="img-responsive text-center center-block logoapropos" alt="Responsive image">
+									<p class="orange text-center"><B>Bienvenue sur Agora.orange</B><br><br> Agora est une idée née suite à la fermeture de feu T411, un site qui aura berçé nos débuts dans ce monde fantastique qu'est Internet. Nous avons eu l'idée de concevoir un site de Cloud où vous choisissez le contenu que vous souhaitez garder pour vous ou partager au reste de la communauté. Agora étant un petit site privé, nous vous demandons de rester discret sur ce dernier afin de restreindre la communauté. <br><br> Nous vous souhaitons une agréable visite !</p>
+								</article>
+					</div>
+					<div>
+						<input id="ac-6" name="accordion-6" type="checkbox" />
+							<label for="ac-6" class="ovr">Sport</label>
+								<article class="ac-large350">
+									<img src="" class="img-responsive text-center center-block logoapropos" alt="Responsive image">
+									<p class="orange text-center"><B>Bienvenue sur Agora.orange</B><br><br> Agora est une idée née suite à la fermeture de feu T411, un site qui aura berçé nos débuts dans ce monde fantastique qu'est Internet. Nous avons eu l'idée de concevoir un site de Cloud où vous choisissez le contenu que vous souhaitez garder pour vous ou partager au reste de la communauté. Agora étant un petit site privé, nous vous demandons de rester discret sur ce dernier afin de restreindre la communauté. <br><br> Nous vous souhaitons une agréable visite !</p>
+								</article>
+					</div>
+				
+				<br>
+
+			</section>			
+
+		</div>
+
+
+
+		<div class="col-6">
+			<section class="ac-container">
+					<div>
+						<input id="ac-2" name="accordion-2" type="checkbox" checked/>
+							<label for="ac-2" class="ovr">Fichiers Experts 2017</label>
+								<article class="ac-large350">
+									<img src="" class="img-responsive text-center center-block logoapropos" alt="Responsive image">
+									<p class="orange text-center"><B>Bienvenue sur Agora.orange</B><br><br> Agora est une idée née suite à la fermeture de feu T411, un site qui aura berçé nos débuts dans ce monde fantastique qu'est Internet. Nous avons eu l'idée de concevoir un site de Cloud où vous choisissez le contenu que vous souhaitez garder pour vous ou partager au reste de la communauté. Agora étant un petit site privé, nous vous demandons de rester discret sur ce dernier afin de restreindre la communauté. <br><br> Nous vous souhaitons une agréable visite !</p>
+								</article>
+					</div>
+
+					<div>
+						<input id="ac-7" name="accordion-7" type="checkbox" checked/>
+							<label for="ac-7" class="ovr">Certificats</label>
+								<article class="ac-largePR">
+									<table class="table ovr">
+									  <thead class="thead-inverse text-center">
+									    <tr>
+									      <th>Document</th>
+									      <th>Année Universitaire</th>
+									      <th>Téléchargement</th>
+									    </tr>
+									  </thead>
+									  <tbody class="text-center">
+									    <tr>
+									      <th scope="row">Certificat de scolarité</th>
+									      <td>2016-2017</td>
+									      <td><button>DL</button></td>
+									    </tr>
+									    <tr>
+									      <th scope="row">Attestation sécurité sociale</th>
+									      <td>2016-2017</td>
+									      <td><button>DL</button></td>
+									    </tr>
+									    <tr>
+									      <th scope="row">Certificat de scolarité</th>
+									      <td>2017-2018</td>
+									      <td><button>DL</button></td>
+									    </tr>
+									    <tr>
+									      <th scope="row">Attestation sécurité sociale</th>
+									      <td>2017-2018</td>
+									      <td><button>DL</button></td>
+									    </tr>
+									    <tr>
+									      <th scope="row">Certificat ImaginR</th>
+									      <td>2017-2018</td>
+									      <td><button>DL</button></td>
+									    </tr>
+									  </tbody>
+									</table>
+								</article>
+					</div>
+
+			</section>
+
+		</div>
+	</div>
+</div>

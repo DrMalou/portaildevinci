82332b739117ddd21d05e5aa6354e84cb183aebc
aliastim <33629825+aliastim@users.noreply.github.com>
2017-11-15 14:34:44 +0100

    a

--- menu.php
+++ menu.php
@@ -17,5 +17,109 @@
 
 
 
+<div class="row grandblock">
+
+
+
+		<div class="col-md-12 navigation1">
+		  <div class="container"><div class="navbar-header">
+		    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+		      <span class="sr-only">Toggle navigation</span>
+		      <span class="icon-bar"></span>
+		      <span class="icon-bar"></span>
+		      <span class="icon-bar"></span>
+		    </button><a class="navbar-brand" href="#">Portail Leonard De Vinci</a>
+		  </div>
+		  <div class="collapse navbar-collapse" id="navbar-ex-collapse">
+		    <ul class="nav navbar-nav navbar-right">
+		      <li class="active"><a href="#">Mails</a></li>
+		      <li><a href="#">More</a>
+		      </li>
+		    </ul>
+		  </div>
+		</div>
+		</div>
+
+
+		<br>
+		<br>
+
+
+
+
+<div class="col-md-12 blockcentre">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><span>
+            <img src="img/logoiim.png">
+          </span></a>
+         <br>
+         <div class="container">
+         <h2 class="grandtitre"> Tableau de Bord de  Monsieur Valentin Genauzeau </h2>
+       </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-ex-collapse">
+          <ul class="nav navbar-nav navbar-right"></ul>
+        </div>
+      </div>
+    </div>
+</div>
+
+
+<br>
+<br>
+<br>
+<br>
+
+
+
+
+<div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <ul class="nav nav-pills">
+              <li class="active">
+                <a href="#">Fiche Etudiant</a>
+              </li>
+              <li>
+                <a href="#">Mon E.D.T</a>
+              </li>
+              <li>
+                <a href="#">Absences</a>
+              </li>
+              <li>
+                <a href="">Mes Notes</a>
+              </li>
+              <li>
+                <a href="#">Mes Cours</a>
+              </li>
+              <li>
+                <a href="">Documents</a>
+              <li>
+                <a href="">Stages</a>
+              <li>
+                <a href="">International</a>
+              </li>
+              <li>
+                <a href="">Rendus Pedagogique</a>
+              </li>
+              <li>
+              <a href="">Finances</a>
+            </li>
+
+
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
 
 

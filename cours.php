26357d2d5f367b0d9770add9ab6ddd04b5c3f6b9
PhilibertHansPorter <33629870+PhilibertHansPorter@users.noreply.github.com>
2017-11-15 17:11:48 +0100

    page cours avec le css dans style
    walabok

--- cours.php
+++ cours.php
@@ -1,29 +1,14 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Mes cours</title>
-    <link rel="stylesheet" type="text/css" href="cours.css">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://use.fontawesome.com/aa5fbf6fe2.js"></script>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
+<?php require 'menu.php' ?>
+<br>
 <div class="row">
-<div class="col-md-10 pagecours">
 
-
+<div class="col-10 pagecours">
     <h1 class="titrecours">Mes cours</h1>
-    <div class="col-md-2">
+    <div class="row">
+
+
+    <div class="col-2">
         <div class="row groupegauche1"><i class="fa fa-book fa-2x" aria-hidden="true"></i>
         <ul>
 <li><h4>2017</h4></li>
@@ -42,7 +27,7 @@
 
 
     </div>
-    <div class="col-md-8">
+    <div class="col-8">
         <h3>IIM - Année 2 - Axe Développement web</h3>
         <table class="table">
             <thead class="thead bleu">
@@ -122,9 +107,10 @@
             </tbody>
         </table>
 
-
+    </div>
     </div>
 </div>
 </div>
+<script src="https://use.fontawesome.com/aa5fbf6fe2.js"></script>
 </body>
 </html>
